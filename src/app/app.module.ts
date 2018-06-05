920128304bb65efa0b6a05d12c4fe91a81434ab8
Dav <dav2070@web.de>
2018-06-05 23:05:46 +0200

    Add router, ng-bootstrap and add start page and navbar in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router } from '@angular/router';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  	declarations: [
+		AppComponent,
+		StartPageComponent
+  	],
+  	imports: [
+		BrowserModule,
+		RouterModule.forRoot([
+			{path: '', component: StartPageComponent}
+		], {
+			useHash: false
+		}),
+		NgbModule.forRoot()
+  	],
+  	providers: [],
+  	bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/start-page/start-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: "calendo-start-page",
+   templateUrl: "./start-page.component.html",
+   styleUrls: [
+      "./start-page.component.css"
+   ]
+})
+export class StartPageComponent{
+
+   constructor(){
+		
+   }
+}
