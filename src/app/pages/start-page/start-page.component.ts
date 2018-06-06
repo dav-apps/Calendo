6fe8daca4aa4a932a34b0d0efc7db5d44744f9f2
Dav <dav2070@web.de>
2018-06-06 02:08:43 +0200

    Use SCSS, make some general improvements and add appointments and todos cards on the start page

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
+declare var $: any;
 
 @Component({
    selector: "calendo-start-page",
    templateUrl: "./start-page.component.html",
    styleUrls: [
-      "./start-page.component.css"
+      "./start-page.component.scss"
    ]
 })
 export class StartPageComponent{
 
    constructor(){
 		
+   }
+
+   ngOnInit(){
+      $('.todo-checkbox').iCheck({
+         checkboxClass: 'icheckbox_square-blue',
+         radioClass: 'iradio_square',
+         increaseArea: '10%'
+      });
    }
 }
