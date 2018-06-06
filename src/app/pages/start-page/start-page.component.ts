f6d3799dd59bef586c42650b2a57e965b0548cb1
Dav <dav2070@web.de>
2018-06-07 01:51:40 +0200

    Add moment and fontawesome packages, improve the design of the appointments and todos cards and add cards for the next six days with the appointments and todos below

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 declare var $: any;
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
 
 @Component({
    selector: "calendo-start-page",
@@ -9,16 +12,47 @@ declare var $: any;
    ]
 })
 export class StartPageComponent{
+   appointments: string = "invisible";
 
    constructor(){
-		
+      fontawesome.library.add(solid);
    }
 
    ngOnInit(){
-      $('.todo-checkbox').iCheck({
-         checkboxClass: 'icheckbox_square-blue',
-         radioClass: 'iradio_square',
-         increaseArea: '10%'
-      });
+      setTimeout(() => {
+         $('.todo-checkbox').iCheck({
+            checkboxClass: 'icheckbox_square-blue',
+            radioClass: 'iradio_square',
+            increaseArea: '10%'
+         });
+      }, 10);
+   }
+
+   GetWeekDay(index: number): string{
+      moment.locale('en');
+      return moment().add(index, 'days').format('dddd');
+   }
+
+   GetDate(index: number): string{
+      moment.locale('en');
+      return moment().add(index, 'days').format('D. MMMM YYYY');
+   }
+
+   ShowOrHideAppointmentsOfDay(day: number){
+      var elementId = "#appointments-day-" + day;
+      if($(elementId).is(":visible")){
+         $(elementId).hide();
+      }else{
+         $(elementId).show();
+      }
+   }
+
+   ShowOrHideTodosOfDay(day: number){
+      var elementId = "#todos-day-" + day;
+      if($(elementId).is(":visible")){
+         $(elementId).hide();
+      }else{
+         $(elementId).show();
+      }
    }
 }
