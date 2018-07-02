42e41630bc30be522ca2174fe10b0d808913cfc7
Dav <dav2070@web.de>
2018-07-02 19:44:41 +0200

    Make some small changes to make the app work in production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: true,
 	apiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
-	baseUrl: "https://calendo-dav.azurewebsites.net",
+	baseUrl: "https://calendo.dav-apps.tech",
 	appId: 5,
 	todoTableId: 10,
 	appointmentTableId: 11,
