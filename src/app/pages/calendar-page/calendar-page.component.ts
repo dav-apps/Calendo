1301d811689d3560fe0dee517b54199a8f58c042
Dav <dav2070@web.de>
2018-07-17 00:06:43 +0200

    Fill the calendarDays array with the days and show it in the calendar

--- src/app/pages/calendar-page/calendar-page.component.ts
+++ src/app/pages/calendar-page/calendar-page.component.ts
@@ -14,27 +14,69 @@ export class CalendarPageComponent{
    calendarDayHeight: number = this.calendarHeight / 5;
    calendarDayWidth: number = window.innerWidth / 7;
    calendarDays = [
-      new Array(7),
-      new Array(7),
-      new Array(7),
-      new Array(7),
-      new Array(7)
+      [{}, {}, {}, {}, {}, {}, {}],
+      [{}, {}, {}, {}, {}, {}, {}],
+      [{}, {}, {}, {}, {}, {}, {}],
+      [{}, {}, {}, {}, {}, {}, {}],
+      [{}, {}, {}, {}, {}, {}, {}]
    ];
    currentMonth: number = 1;
    currentYear: number = 2018;
+   firstWeekDayOfMonth = 0;
 
    constructor(){}
 
    ngOnInit(){
       this.setSize();
+      this.initialize();
+   }
+
+   initialize(){
+      var date = new Date();
+      this.currentMonth = date.getMonth();
+      this.currentYear = date.getFullYear();
+
       this.createDays();
    }
 
    createDays(){
       // Get the first day of the month
-      var date = new Date(moment().unix() * 1000);
-      this.currentMonth = date.getMonth() + 1;
-      this.currentYear = date.getFullYear();
+      var date = new Date();
+      date.setMonth(this.currentMonth);
+      date.setFullYear(this.currentYear);
+
+      var firstOfMonth = moment.unix(date.getTime() / 1000).startOf('month');
+      var currentDay = $.extend(true, {}, firstOfMonth);
+      this.firstWeekDayOfMonth = firstOfMonth.day();
+
+      if(this.firstWeekDayOfMonth == 0){
+         this.firstWeekDayOfMonth = 6;
+      }else{
+         this.firstWeekDayOfMonth = this.firstWeekDayOfMonth - 1;
+      }
+
+      for (let i = 0; i < firstOfMonth.daysInMonth(); i++) {
+         this.addDateToCalendarDaysArray(i, currentDay);
+         currentDay.add(1, 'days');
+      }
+   }
+
+   addDateToCalendarDaysArray(index: number, date: moment.Moment){
+      var obj = {
+         day: date.format("D")
+      }
+
+      index = index + this.firstWeekDayOfMonth;
+      var rowIndex = Math.floor(index / 7);
+      var columnIndex = index % 7;
+
+      if(this.calendarDays[rowIndex]){
+         this.calendarDays[rowIndex][columnIndex] = obj;
+      }
+   }
+
+   getMonthName(index: number): string{
+      return moment.months(index);
    }
 
    onResize(event: any){
@@ -46,7 +88,7 @@ export class CalendarPageComponent{
                            - $("#calendo-navbar").height()
                            - $("#calendar-top-bar").height()
                            - $("#calendar-label-div").height()
-                           - 42;
+                           - 75;
 
       this.calendarDayHeight = this.calendarHeight / 5;
       this.calendarDayWidth = window.innerWidth / 7;
