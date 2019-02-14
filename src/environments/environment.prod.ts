0ac8ea2534a8f028ceed69df5c7477ef31627dfc
Dav <dav2070@web.de>
2019-02-14 15:09:52 +0100

    Change the version to 0.5.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	apiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	baseUrl: "https://calendo.dav-apps.tech",
-	version: "0.5.1",
+	version: "0.5.2",
 	appId: 5,
 	todoTableId: 10,
 	appointmentTableId: 11,
