d702df10f59b0c335b892d7adc47f2f1fb07a4b3
David <dav2070@web.de>
2019-05-15 21:42:08 +0200

    Use the Fluent checkbox and improve the design

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -15,32 +15,10 @@ export class TodoItemComponent{
    @Input() todo: Todo = new Todo("", false, 0, "", []);
    @Input() showBadge: boolean = true;
    @Output() delete = new EventEmitter();
-
-   constructor(){}
-
-   ngOnInit(){
-      setTimeout(() => {
-         var todoItemClass = '.todo-item-' + this.todo.uuid;
-
-         $(todoItemClass).iCheck({
-            checkboxClass: 'icheckbox_square-blue',
-            radioClass: 'iradio_square'
-         });
-         
-         if(this.todo.completed){
-            $(todoItemClass).iCheck('check');
-         }
-
-         $(todoItemClass).on('ifChecked', (event) => {
-            this.todo.SetCompleted(true);
-            $(todoItemClass).iCheck('check');
-         });
-         $(todoItemClass).on('ifUnchecked', (event) => {
-            this.todo.SetCompleted(false);
-            $(todoItemClass).iCheck('uncheck');
-         });
-      }, 10);
-   }
+	
+	ToggleCheckbox(){
+		this.todo.SetCompleted(!this.todo.completed);
+	}
 
    Delete(){
       this.todo.Delete();
