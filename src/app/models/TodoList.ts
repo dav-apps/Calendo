e3391c97e9c66fe0b41996cdecabfc51645798b3
David <dav2070@web.de>
2019-05-24 19:05:40 +0200

    Add uuid to constructor and add functions for getting all Todo Lists

--- src/app/models/TodoList.ts
+++ src/app/models/TodoList.ts
@@ -1,20 +1,22 @@
-import { Todo } from './Todo';
-import { GetTableObject, TableObject } from 'dav-npm';
+import { Todo, ConvertTableObjectToTodo } from './Todo';
+import { GetTableObject, TableObject, GetAllTableObjects } from 'dav-npm';
 import { environment } from '../../environments/environment';
 
 export class TodoList{
-   public uuid: string;
-   public name: string;
-   public time: number;
-	public todos: Todo[];
-	public groups: string[];
+   public uuid: string = null;
+   public name: string = "";
+   public time: number = 0;
+	public todos: Todo[] = [];
+	public groups: string[] = [];
 
    constructor(
+      uuid: string,
 		name: string,
       time?: number,
       todos?: Todo[],
       groups?: string[]
 	){
+      this.uuid = uuid;
       this.name = name;
       this.time = time ? time : 0;
       this.todos = todos ? todos : [];
@@ -22,7 +24,7 @@ export class TodoList{
    }
 
 	public static async Create(name: string, time: number = 0, todos: Todo[] = [], groups: string[] = []) : Promise<TodoList>{
-		let list = new TodoList(name, time, todos, groups);
+		let list = new TodoList(null, name, time, todos, groups);
 		await list.Save();
 		return list;
 	}
@@ -51,4 +53,57 @@ export class TodoList{
 			{ name: environment.todoListGroupsKey, value: this.groups.join(',') }
 		]);
 	}
+}
+
+export async function GetAllTodoLists() : Promise<TodoList[]>{
+	let tableObjects = await GetAllTableObjects(environment.todoListTableId, false);
+	let todoLists: TodoList[] = [];
+
+	for(let tableObject of tableObjects){
+		let todoList = await ConvertTableObjectToTodoList(tableObject);
+
+		if(todoList){
+			todoLists.push(todoList);
+		}
+	}
+
+	return todoLists;
+}
+
+export async function ConvertTableObjectToTodoList(tableObject: TableObject) : Promise<TodoList>{
+	if(tableObject.TableId != environment.todoListTableId) return null;
+
+	let name: string = "";
+	name = tableObject.GetPropertyValue(environment.todoListNameKey);
+
+	let time: number = 0;
+	let todoTimeString = tableObject.GetPropertyValue(environment.todoListTimeKey);
+	if(todoTimeString){
+		time = Number.parseInt(todoTimeString);
+	}
+
+	let todos: Todo[] = [];
+	let todoUuidsString = tableObject.GetPropertyValue(environment.todoListTodosKey);
+	
+	if(todoUuidsString){
+		for(let uuid of todoUuidsString.split(',')){
+			// Get the todo from the local storage
+			let todoTableObject = await GetTableObject(uuid);
+			let todo = ConvertTableObjectToTodo(todoTableObject);
+			if(todo){
+				todos.push(todo);
+			}
+		}
+	}
+
+	let groups: string[] = [];
+	let groupsString = tableObject.GetPropertyValue(environment.todoListGroupsKey);
+	
+	if(groupsString){
+		for(let group of groupsString.split(',')){
+			groups.push(group);
+		}
+	}
+
+	return new TodoList(tableObject.Uuid, name, time, todos, groups);
 }
