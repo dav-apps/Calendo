d14581209067ca5d261d91df41fed64bab02b348
David <dav2070@web.de>
2020-05-05 16:58:03 +0200

    Change the version to 0.8.5

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
    production: false,
 	apiKey: "eUzs3PQZYweXvumcWvagRHjdUroGe5Mo7kN1inHm",
 	baseUrl: "http://localhost:3002",
-	version: "0.8.4",
+	version: "0.8.5",
    appId: 5,
    todoListTableId: 11,
 	todoTableId: 12,
