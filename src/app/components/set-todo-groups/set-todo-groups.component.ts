0b1902c70fdd2faabfc4b6569e986aa32720e656
David <dav2070@web.de>
2020-10-23 23:21:32 +0200

    Fix some bugs with groups
    - Fix a bug which caused to show no groups when editing a todo list
    - Disable the ability to add groups with empty name

--- src/app/components/set-todo-groups/set-todo-groups.component.ts
+++ src/app/components/set-todo-groups/set-todo-groups.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 import { DataService } from 'src/app/services/data-service'
 import { GetAllTodoGroups } from '../../models/Todo'
 import { enUS } from '../../../locales/locales'
@@ -12,9 +12,9 @@ export class SetTodoGroupsComponent{
 	faPlus = faPlus
 	locale = enUS.setTodoGroups
    existingGroups: string[] = []
-   todoGroups: string[] = []
-   @Output()
-   groupsChanged = new EventEmitter<string[]>()
+	todoGroups: string[] = []
+	@Input() initialGroups: string[] = []
+   @Output() groupsChanged = new EventEmitter<string[]>()
 	newGroupName: string = ""
 	groupTextFieldStyle = {
 		root: {
@@ -28,20 +28,36 @@ export class SetTodoGroupsComponent{
       this.locale = this.dataService.GetLocale().setTodoGroups;
 		this.GetAllTodoGroups();
 	}
+
+	ngAfterViewInit() {
+		GetAllTodoGroups();
+
+		// Add the initial groups to the groups
+		this.initialGroups.forEach(group => {
+			this.todoGroups.push(group)
+
+			// If existingGroups contains this group, remove it there
+			let i = this.existingGroups.indexOf(group)
+			if (i != -1) {
+				this.existingGroups.splice(i, 1)
+			}
+		})
+	}
 	
-	async GetAllTodoGroups(){
+	async GetAllTodoGroups() {
       this.existingGroups = [];
       var todoGroups = await GetAllTodoGroups();
       todoGroups.forEach(group => {
          // Check if the group is already selected
          if(this.todoGroups.findIndex(g => g == group) == -1){
-				this.existingGroups.push(group);
+				this.existingGroups.push(group)
          }
-      });
+		})
    }
 
-	AddGroup(name: string){
-      if(this.todoGroups.findIndex(g => g == name) == -1){
+	AddGroup(name: string) {
+		name = name.trim()
+      if(name.length > 0 && this.todoGroups.findIndex(g => g == name) == -1){
          this.todoGroups.push(name);
    
          // Remove the group from existingGroups
@@ -55,7 +71,8 @@ export class SetTodoGroupsComponent{
       }
 	}
 	
-	RemoveGroup(name: string){
+	RemoveGroup(name: string) {
+		name = name.trim()
       var index = this.todoGroups.findIndex(g => g == name);
 
       if(index !== -1){
@@ -71,10 +88,10 @@ export class SetTodoGroupsComponent{
 		this.newGroupName = ''
 	}
 
-	NameChanged(event: InputEvent) {
+	NameChanged() {
 		setTimeout(() => {
 			// Update the groups in the parent element with the group name in the text field
-			if (this.todoGroups.findIndex(g => g == this.newGroupName) == -1) {
+			if (this.newGroupName.trim().length > 0 && this.todoGroups.findIndex(g => g == this.newGroupName) == -1) {
 				this.TriggerGroupsUpdate([...this.todoGroups, this.newGroupName])
 			} else {
 				this.TriggerGroupsUpdate(this.todoGroups)
