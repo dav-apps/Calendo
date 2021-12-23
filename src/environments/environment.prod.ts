279ee86f0980f9e7f6f9026757680718c0b1bd70
David <dav2070@web.de>
2021-12-23 19:55:15 +0100

    Bump to v0.9.8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: true,
 	apiKey: "gHgHKRbIjdguCM4cv5481hdiF5hZGWZ4x12Ur-7v",
 	baseUrl: "https://calendo.dav-apps.tech",
-	version: "0.9.7",
+	version: "0.9.8",
 	appId: 5,
 	todoListTableId: 13,
 	todoTableId: 10,
