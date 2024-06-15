29bb575f59284924b8048d29302c45a206ff4586
David <zurek.dav@gmail.com>
2024-06-15 15:08:42 +0200

    Add calendar & ability to select or deselect date in CreateTodoDialog

--- src/app/dialogs/create-todo-dialog/create-todo-dialog.component.ts
+++ src/app/dialogs/create-todo-dialog/create-todo-dialog.component.ts
@@ -6,22 +6,26 @@ import {
 	ElementRef,
 	EventEmitter
 } from "@angular/core"
+import { DateTime } from "luxon"
 import { Dialog } from "dav-ui-components"
 import { LocalizationService } from "src/app/services/localization-service"
 
 @Component({
 	selector: "calendo-create-todo-dialog",
-	templateUrl: "./create-todo-dialog.component.html"
+	templateUrl: "./create-todo-dialog.component.html",
+	styleUrl: "./create-todo-dialog.component.scss"
 })
 export class CreateTodoDialogComponent {
 	locale = this.localizationService.locale.dialogs.createTodoDialog
 	actionsLocale = this.localizationService.locale.actions
 	@Input() loading: boolean = false
 	@Input() nameError: string = ""
+	@Input() date: DateTime = DateTime.now()
 	@Output() primaryButtonClick = new EventEmitter()
 	@ViewChild("dialog") dialog: ElementRef<Dialog>
 	visible: boolean = false
 	name: string = ""
+	saveDate: boolean = false
 
 	constructor(private localizationService: LocalizationService) {}
 
@@ -44,6 +48,14 @@ export class CreateTodoDialogComponent {
 
 	nameTextfieldChange(event: Event) {
 		this.name = (event as CustomEvent).detail.value
+	}
+
+	calendarChange(event: CustomEvent) {
+		this.date = event.detail.date
+	}
+
+	saveDateCheckboxChange(event: CustomEvent) {
+		this.saveDate = event.detail.checked
 	}
 
 	submit() {
