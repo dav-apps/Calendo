7ec2e6747743dd9d3ee6802325cbf0b5d890243b
David <zurek.dav@gmail.com>
2024-07-19 22:27:03 +0200

    Implement simple list of todo items with drag & drop in TodoListTree

--- src/app/components/todo-list-tree/todo-list-tree.component.ts
+++ src/app/components/todo-list-tree/todo-list-tree.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core"
 import { faPlus } from "@fortawesome/pro-light-svg-icons"
+import { Todo } from "src/app/models/Todo"
 import { TodoList } from "src/app/models/TodoList"
 
 @Component({
@@ -13,4 +14,12 @@ export class TodoListTreeComponent {
 	@Input() showRoot: boolean = false
 	@Input() readonly: boolean = false
 	@Output() update = new EventEmitter()
+
+	todoDragged(event: (Todo | TodoList)[]) {
+		this.todoList.SetItems(event)
+	}
+
+	isItemTodoList(item: Todo | TodoList) {
+		return item instanceof TodoList
+	}
 }
