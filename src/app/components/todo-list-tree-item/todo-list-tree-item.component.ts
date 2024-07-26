3d0cb1c343efc98f4129d7d7e421d62283e95f17
David <zurek.dav@gmail.com>
2024-07-26 13:54:38 +0200

    Add expand / collapse button to sub todo list items

--- src/app/components/todo-list-tree-item/todo-list-tree-item.component.ts
+++ src/app/components/todo-list-tree-item/todo-list-tree-item.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core"
-import { faPlus, faEllipsis } from "@fortawesome/pro-light-svg-icons"
+import {
+	faPlus,
+	faEllipsis,
+	faChevronRight
+} from "@fortawesome/pro-light-svg-icons"
 import { Todo } from "src/app/models/Todo"
 import { TodoList } from "src/app/models/TodoList"
 
@@ -11,10 +15,12 @@ import { TodoList } from "src/app/models/TodoList"
 export class TodoListTreeItemComponent {
 	faPlus = faPlus
 	faEllipsis = faEllipsis
+	faChevronRight = faChevronRight
 	@Input() item: Todo | TodoList
 	@Output() addButtonClick = new EventEmitter()
 	@Output() moreButtonClick = new EventEmitter()
 	subItems: (Todo | TodoList)[] = []
+	expanded: boolean = false
 
 	ngOnInit() {
 		if (this.isItemTodoList) {
@@ -24,5 +30,9 @@ export class TodoListTreeItemComponent {
 
 	isItemTodoList(item: Todo | TodoList) {
 		return item instanceof TodoList
+	}
+
+	chevronButtonClick() {
+		this.expanded = !this.expanded
 	}
 }
