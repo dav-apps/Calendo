0cc92027196fd6b60e38340122c22759fd135e9f
David <zurek.dav@gmail.com>
2024-08-06 22:10:39 +0800

    Fix issue with reflecting changes after updating appointment color in SmallAppointmentItem

--- src/app/components/small-appointment-item/small-appointment-item.component.ts
+++ src/app/components/small-appointment-item/small-appointment-item.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core"
+import {
+	Component,
+	Input,
+	Output,
+	EventEmitter,
+	KeyValueDiffer,
+	KeyValueDiffers
+} from "@angular/core"
 import { DateTime } from "luxon"
 import {
 	TonalPalette,
@@ -6,7 +13,6 @@ import {
 	hexFromArgb
 } from "@material/material-color-utilities"
 import { Appointment } from "src/app/models/Appointment"
-import { DataService } from "src/app/services/data-service"
 import { environment } from "src/environments/environment"
 
 @Component({
@@ -29,17 +35,37 @@ export class SmallAppointmentItemComponent {
 	backgroundColor: string = environment.appointmentDefaultColor
 	textColor: string = environment.appointmentDefaultColor
 	timeSpan: string = ""
+	appointmentDiffer: KeyValueDiffer<string, any>
 
-	constructor(private dataService: DataService) {}
+	constructor(private differs: KeyValueDiffers) {}
 
 	ngOnInit() {
+		this.timeSpan = this.getTimeSpan()
+		this.appointmentDiffer = this.differs.find(this.appointment).create()
+
+		this.loadColor()
+	}
+
+	ngDoCheck() {
+		// Check for any changes in the appointment
+		const changes = this.appointmentDiffer.diff(this.appointment)
+
+		if (changes != null) {
+			changes.forEachChangedItem(change => {
+				if (change.key == "color") {
+					// Reload the color
+					this.loadColor()
+				}
+			})
+		}
+	}
+
+	loadColor() {
 		if (this.appointment.color != null) {
 			let palette = TonalPalette.fromInt(argbFromHex(this.appointment.color))
 			this.backgroundColor = hexFromArgb(palette.tone(90))
 			this.textColor = hexFromArgb(palette.tone(10))
 		}
-
-		this.timeSpan = this.getTimeSpan()
 	}
 
 	getTimeSpan() {
